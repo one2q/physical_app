f24608c24973552217797875c1fa3219a22b3881
Nikolaev Valentin <val8673@gmail.com>
2023-02-05 00:55:30 +0100

    add swagger

--- pht/settings.py
+++ pht/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "blog",
+	"drf_yasg",
 ]
 
 MIDDLEWARE = [
--- pht/urls.py
+++ pht/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from blog.urls import blog_router
+from blog.yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
 urlpatterns += blog_router.urls
+urlpatterns += doc_urls
