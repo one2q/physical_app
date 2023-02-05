70bfc60d260bada5a72a98dd987888797fcfbd14
Nikolaev Valentin <val8673@gmail.com>
2023-02-05 16:13:06 +0100

    del log settings

--- pht/settings.py
+++ pht/settings.py
@@ -92,26 +92,3 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# TODO Убрать не забыть!!!
-LOGGING = {
-    "version": 1,
-    "filters": {
-        "require_debug_true": {
-            "()": "django.utils.log.RequireDebugTrue",
-        }
-    },
-    "handlers": {
-        "console": {
-            "level": "DEBUG",
-            "filters": ["require_debug_true"],
-            "class": "logging.StreamHandler",
-        }
-    },
-    "loggers": {
-        "django.db.backends": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-        }
-    },
-}
