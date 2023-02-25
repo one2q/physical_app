eb54e78426e7a041ee951040f6a8b4c927671982
Nikolaev Valentin <val8673@gmail.com>
2023-02-26 00:04:53 +0100

    fix settings

--- pht/settings.py
+++ pht/settings.py
@@ -7,10 +7,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
-
-DEBUG = os.getenv("DEBUG", True)
-
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+DEBUG = os.getenv("DEBUG", False) == 'True'
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
 
 INSTALLED_APPS = [
 	"django.contrib.admin",
